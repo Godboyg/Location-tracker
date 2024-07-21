f7163021cf0c52698cde318a54fd1bec643065c0
kushal <kushalsinghnegi72@gmail.com>
2024-07-21 09:43:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const { Server } = require("socket.io");
+const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connect",(socket)=>{
+    console.log("user connected");
+    socket.on("send-location",(data)=>{
+        io.emit("recieve-location", {id: socket.id, ...data});
+    })
+
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnected",socket.id);
+    });
+})
+
+app.get("/",(req,res)=>{
+    res.render("index");
+})
+
+server.listen(5000);
